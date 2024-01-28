68db28cfa911f3132c0860c4c88d88d7f517888c
ryanleachacc <157077913+ryanleachacc@users.noreply.github.com>
2024-01-27 23:03:50 -0600

    Update 01_data_types_tests.cpp

--- test/homework/01_data_types/01_data_types_tests.cpp
+++ test/homework/01_data_types/01_data_types_tests.cpp
@@ -1,6 +1,16 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "data_types.h"
+
+int numbers(int num1) {
+	
+    int num2 = 5;
+
+    int result = num1 * num2;
+
+    return result;
+}
+
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
